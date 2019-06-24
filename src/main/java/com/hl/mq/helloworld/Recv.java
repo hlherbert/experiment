ec9f1e13388f65b3f5c3d37d3bdbc89501022083
hlherbert <184624975@qq.com>
2019-06-24 11:44:53 +0800

    mq study

---
+++ src/main/java/com/hl/mq/helloworld/Recv.java
@@ -0,0 +1,52 @@
+package com.hl.mq.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv {
+    private final static String Q_NAME = "hello";
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = null;
+        try {
+            connection = connectionFactory.newConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        Channel channel = null;
+        try {
+            channel = connection.createChannel();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            channel.queueDeclare(Q_NAME, false, false, false, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("[*] Waiting for messages. To exit prect CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String msg = new String(delivery.getBody(), "utf-8");
+            System.out.println("[*] Received '"+msg+"'");
+        };
+
+        try {
+            channel.basicConsume(Q_NAME, true, deliverCallback, consumerTag -> {System.out.println("cancel: cousumerTag="+consumerTag);});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/hl/mq/helloworld/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Learn Rabbit Mq
+ * https://www.rabbitmq.com/tutorials/tutorial-one-java.html
+ * https://www.rabbitmq.com/java-client.html
+ */
+package com.hl.mq.helloworld;
