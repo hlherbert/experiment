fbd873849d6b4e13a13d171572cc63e9036e6fcd
hlherbert <184624975@qq.com>
2019-07-22 19:01:00 +0800

    嵌套绘图

---
+++ src/main/java/com/hl/algo/dividen/DividenPanel.java
@@ -0,0 +1,118 @@
+package com.hl.algo.dividen;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DividenPanel extends JPanel {
+
+    private int MAX_DEPTH = 3;
+    private boolean overlap = false;
+    private boolean[] b=new boolean[4];
+    private List<Shape> shapes = new LinkedList<Shape>();
+
+    private DividenPanel() {
+        init();
+    }
+
+    private void dividenFunc(int depth, float x, float y, float w, float h) {
+        if (depth < 0) {
+            return;
+        }
+
+        if (overlap || depth==0) {
+            if (b[0]) {
+                Shape line = new Line2D.Float(x, y, x + w, y + h);
+                shapes.add(line);
+            }
+            if (b[1]) {
+                Shape line = new Line2D.Float(x, y + h, x + w, y);
+                shapes.add(line);
+            }
+            if (b[2]) {
+                Rectangle2D.Float rect = new Rectangle2D.Float(x + w / 4, y + h / 4, w / 2, h / 2);
+                shapes.add(rect);
+            }
+            if (b[3]) {
+                Eclipse2D.Float eclipse = new Eclipse2D.Float(x+w/2,y+h/2,w/2, h/2);
+                shapes.add(eclipse);
+            }
+        }
+
+        depth--;
+        float dw = w/2;
+        float dh = h/2;
+        dividenFunc(depth,x,y,dw,dh);
+        dividenFunc(depth,x+dw,y,dw,dh);
+        dividenFunc(depth,x,y+dh,dw,dh);
+        dividenFunc(depth,x+dw,y+dh,dw,dh);
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g1 = (Graphics2D)g;
+        g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+        shapes.clear();
+        dividenFunc(MAX_DEPTH,0,0,w,h);
+        for (Shape shape: shapes) {
+            g1.draw(shape);
+        }
+    }
+
+    public void init() {
+        JButton btn = new JButton("+depth");
+        this.add(btn);
+        btn.addActionListener((e)->{
+            MAX_DEPTH++;
+            this.repaint();
+        });
+
+        JButton btn2 = new JButton("-depth");
+        this.add(btn2);
+        btn2.addActionListener((e)->{
+            if (MAX_DEPTH > 0)
+                MAX_DEPTH--;
+            this.repaint();
+        });
+
+        JButton btn3 = new JButton("overlap");
+        this.add(btn3);
+        btn3.addActionListener((e)->{
+            overlap = !overlap;
+            this.repaint();
+        });
+
+        for (int i=0;i<b.length;i++) {
+            JButton btnShape = new JButton(Integer.toString(i));
+
+            this.add(btnShape);
+            btnShape.addActionListener((e)->{
+                int index = Integer.valueOf(btnShape.getText());
+                b[index] = !b[index];
+                this.repaint();
+            });
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frm = new JFrame("Dividen");
+        frm.setSize(500,500);
+        DividenPanel dividenPanel = new DividenPanel();
+        frm.setContentPane(dividenPanel);
+        frm.setVisible(true);
+    }
+}
---
+++ src/main/java/com/hl/algo/dividen/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 用分治法嵌套绘图。发现图形的模式。
+ *
+ * 初始条件：深度depth = MAX_DEPTH
+ * 1.将绘图步骤F(R)应用到矩形R
+ * 2.将R以中线分割为4个大小相同的子矩形
+ * 3.depth := depth-1
+ * 4.对4个子矩形作为R，重复以上步骤, 直到深度depth=0。
+ */
+package com.hl.algo.dividen;
